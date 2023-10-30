4773cfad6d00f244e846dd1f63acc291d8c98acf
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 17:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function insert_Row() {
 	let tr=document.createElement("tr");
 	let cell=document.createElement("td");
 	let cell2=document.createElement("td");
-	cell.innerText="New Cell1";
+	cell.innerText="New Cell1Row1 Cell1";
 	cell2.innerText="New Cell2";
 	tr.appendChild(cell);
 	tr.appendChild(cell2);

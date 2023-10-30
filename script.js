9a844b375d1a39f9bedad613a31e2f4a39b29259
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 16:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let row=document.querySelector("tr");
+let table=document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code here
-  
-  
+	let tr=document.createElement("tr");
+	let cell=document.createElement("td");
+	let cell2=document.createElement("td");
+	cell.innerText="New Cell1";
+	cell2.innerText="New Cell2";
+	tr.appendChild(cell);
+	tr.appendChild(cell2);
+	table.insertBefore(tr, table.children[0]);
 }
